e7f330cf4d95f95939be43f032b6e1865d65f7c4
jaxdsout <jaxon.southern@mac.com>
2024-05-22 14:49:11 -0500

    bootstrap install, adding pics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,47 +7,55 @@ import {ReactComponent as LiveLink} from "./components/link.svg"
 import { Link } from 'react-router-dom';
 
 
+
+
 function App() {
   return (
     <div>
       <h1> portfolio </h1>
-      <div>
+      <div className='container text-container'>
         <h2> projects </h2>
-        <div>
+        <div className="row">
+          <div className='col'>
           <h3> gallery website</h3>
           <img src="" alt='img'></img>
-          <Link to="https://github.com/jaxdsout/gallery-site"> <Code /></Link>
-        </div>
-        <div>
-          <h3> substream</h3>
-          <img src="" alt='img'></img>
-          <Link to="https://github.com/jaxdsout/substream"> <Code /> </Link>
-          <Link to="https://jaxdsout.github.io/substream/"> <LiveLink /></Link>
-        </div>
-        <div>
-          <h3> qwiklok</h3>
-          <img src="" alt='img'></img>
-          <Link to="https://github.com/jaxdsout/qwiklok"> <Code /></Link>
-        </div>
-        <div>
-          <h3> simon</h3>
-          <img src="" alt='img'></img>
-          <Link to="https://github.com/jaxdsout/simon"> <Code /></Link>
-          <Link to="https://jaxdsout.github.io/simon/"> <LiveLink /></Link>
-
+          <Link to="https://github.com/jaxdsout/gallery-site" target="_blank" rel="noopener noreferrer"> <Code /></Link>
+          </div>
+          <div className='col'>
+            <h3> substream</h3>
+            <img src="/images/substream.png" className='img-thumbnail' alt='img'></img>
+            <Link to="https://github.com/jaxdsout/substream" target="_blank" rel="noopener noreferrer"> <Code /> </Link>
+            <Link to="https://jaxdsout.github.io/substream/" target="_blank" rel="noopener noreferrer"> <LiveLink /></Link>
+          </div>
+          <div className='col'>
+            <h3> qwiklok</h3>
+            <img src="/images/qwiklok.png" className='img-thumbnail' alt='img'></img>
+            <Link to="https://github.com/jaxdsout/qwiklok" target="_blank" rel="noopener noreferrer"> <Code /></Link>
+          </div>
+          <div className='col'>
+            <h3> simon</h3>
+            <img src="/images/simon.png" className='img-thumbnail' alt='img'></img>
+            <Link to="https://github.com/jaxdsout/simon" target="_blank" rel="noopener noreferrer"> <Code /></Link>
+            <Link to="https://jaxdsout.github.io/simon/" target="_blank" rel="noopener noreferrer"> <LiveLink /></Link>
+          </div>
         </div>
       </div>
-      <div>
-        <h2> contact </h2>
-        <Link to={"mailto:jaxon.southern@mac.com"}>
-          <Email />
-        </Link>
-        <Link to={"http://www.github.com/jaxdsout"}>
-          <GitHub />
-        </Link>
-        <Link to={"https://www.linkedin.com/in/jaxonsouthern/"}>
-          <LinkedIn />
-        </Link>
+
+      <div className='container text-container'>
+        <div className='row'>
+          <div className='col'>
+          <h2> contact </h2>
+            <Link to={"mailto:jaxon.southern@mac.com"}>
+            <Email />
+          </Link>
+          <Link to={"http://www.github.com/jaxdsout"}>
+            <GitHub />
+          </Link>
+          <Link to={"https://www.linkedin.com/in/jaxonsouthern/"}>
+            <LinkedIn />
+          </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
