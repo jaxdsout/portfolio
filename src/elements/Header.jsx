50a1be6b05ab46fab10dd100631d065c91fccb68
jaxdsout <jaxon.southern@mac.com>
2024-05-24 14:42:20 -0500

    favi

--- src/elements/Header.jsx
+++ src/elements/Header.jsx
@@ -4,7 +4,7 @@ function Header () {
 
     return (
         <div className='container text-container text-center'>
-            <h1 className='text-white raleway-heavy header'> portfolio </h1>
+            <h1 className='text-white raleway-heavy header'> portfolio  __   js</h1>
             <p className='text-white jersey-25-charted-regular author'>jaxon southern</p>
         </div>
     )
