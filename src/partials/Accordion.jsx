5c0288e886705912007b014fc2f65b8b838e2a07
jaxdsout <jaxon.southern@mac.com>
2024-05-24 12:42:37 -0500

    pages populate

--- src/partials/Accordion.jsx
+++ src/partials/Accordion.jsx
@@ -1,77 +1,24 @@
 import { Link } from "react-router-dom"
+import Projects from "./Projects"
 
 function Accordion({ Repo, LiveLink, Resume, Email, LinkedIn, GitHub }) {
   return (
     <div className="accordion accordion-flush" id="accordionFlushExample">
         
         <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-              <h2 className='raleway-regular'> projects </h2>
-            </button>
-          </h2>
-          <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              <div className="row d-flex align-items-end my-2 overflow-x-auto">
-                <div className='col'>
-                  <img src="/images/gallery.png" className='rounded img-fluid' alt='img'></img>
-                  <div className='d-flex justify-content-between mt-3 align-items-center'>
-                  <h3 className='raleway-medium text-center title'> the gallery </h3>
-                    <div>
-                      <Link to="https://github.com/jaxdsout/gallery-site" target="_blank" rel="noopener noreferrer"> 
-                      <Repo className='sub-icon'/>
-                    </Link>
-                    </div>
-                    
-                  </div>
+            <h2 className="accordion-header">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    <h2 className='raleway-regular'> projects </h2>
+                </button>
+            </h2>
+            <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div className="accordion-body">
+                    <Projects 
+                        LiveLink={LiveLink}
+                        Repo={Repo}
+                    />
                 </div>
-                <div className='col'>
-                  <img src="/images/substream.png" className='rounded img-fluid' alt='img'></img>
-                  <div className='d-flex justify-content-between mt-3 align-items-center'>
-                    <h3 className='raleway-medium text-center title'> substream</h3>
-                    <div>
-                      
-                      <Link to="https://github.com/jaxdsout/substream" target="_blank" rel="noopener noreferrer"> 
-                      <Repo className='sub-icon'/>
-                    </Link>
-                    <Link to="https://jaxdsout.github.io/substream/" target="_blank" rel="noopener noreferrer"> 
-                      <LiveLink className='sub-icon'/>
-                    </Link>
-                    </div>
-                    
-                  </div>                  
-                </div>
-                <div className='col'>
-                  <img src="/images/qwiklok.png" className='rounded img-fluid' alt='img'></img>
-                  <div className='d-flex justify-content-between mt-3 align-items-center'>
-                    <h3 className='raleway-medium text-center title'> qwiklok</h3>
-                    <div>
-                       <Link to="https://github.com/jaxdsout/qwiklok" target="_blank" rel="noopener noreferrer"> 
-                        <Repo className='sub-icon'/>
-                      </Link>
-                    </div>
-                     
-                  </div>
-                </div>
-                <div className='col'>
-                  <img src="/images/simon.png" className='rounded img-fluid' alt='img'></img>
-                  <div className='d-flex justify-content-between mt-3 align-items-center'>
-                    <h3 className='raleway-medium text-center title'> simon</h3>
-                    <div>
-                      <Link to="https://github.com/jaxdsout/simon" target="_blank" rel="noopener noreferrer"> 
-                      <Repo className='sub-icon'/>
-                    </Link>
-                    <Link to="https://jaxdsout.github.io/simon/" target="_blank" rel="noopener noreferrer"> 
-                      <LiveLink className='sub-icon'/>
-                    </Link>
-                    </div>
-                    
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
-          
         </div>
 
         <div className="accordion-item">
@@ -86,7 +33,7 @@ function Accordion({ Repo, LiveLink, Resume, Email, LinkedIn, GitHub }) {
               <p> a little blurb about me and my story. </p>
 
               </div>
-              <div className="text-center d-flex">
+              <div className="text-center d-flex align-items-center">
                 <Link to="/images/JaxonSouthernResume.pdf" target="_blank" rel="noopener noreferrer"> 
                     <Resume className='icon'/>
                 </Link>
