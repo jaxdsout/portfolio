e94c539a2db4e9db9e9352dec5254e19bfb326ef
jaxdsout <jaxon.southern@mac.com>
2024-10-22 20:13:32 -0500

    portfolio redesign; removed accordion and added wave background

--- src/elements/Contact.jsx
+++ src/elements/Contact.jsx
@@ -1,22 +1,18 @@
 import { Link } from "react-router-dom"
-import linkedin from '../components/linkedin.svg'
-import github from '../components/github.svg'
-import email from '../components/email.svg'
 
 function Contact () {
 
     return (
-      <div className='container text-center mt-3'>
+      <div className='container text-center mt-4'>
         <Link className='' to={"mailto:jaxon.southern@mac.com"}>
-          <img src={email} alt="email" className="icon" title="Email"/>
+          <i className="bi bi-linkedin dom-icon" title="Email"></i>
         </Link>
         <Link className='' to={"http://www.github.com/jaxdsout"} target="_blank" rel="noopener noreferrer">
-          <img src={github} alt="github" className="icon" title="Github"/>
+          <i className="bi bi-github dom-icon" title="GitHub"></i>
         </Link>
         <Link className='' to={"https://www.linkedin.com/in/jaxonsouthern/"} target="_blank" rel="noopener noreferrer">
-          <img src={linkedin} alt="linkedin" className="icon" title="LinkedIn"/>
+          <i className="bi bi-send-fill dom-icon" title="LinkedIn"></i>
         </Link>
-        <hr className="mt-4"/>
       </div>
     )
 }
--- src/elements/Header.jsx
+++ src/elements/Header.jsx
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom"
 
 function Header () {
 
     return (
         <div className='container text-container text-center head'>
-            <h1 className='text-white raleway-heavy header'> portfolio </h1>
+            <Link className="header" to="/"><h1 className='text-white raleway-heavy header'> portfolio </h1></Link>
             <p className='text-white jersey-25-charted-regular author'>jaxon southern</p>
         </div>
     )
