34e4f7a8461c43a7ef84944c51085c9add1ae0bb
jaxdsout <jaxon.southern@mac.com>
2025-02-04 18:57:36 -0600

    flex grow for about area

--- src/elements/About.jsx
+++ src/elements/About.jsx
@@ -2,83 +2,62 @@ function About () {
 
     return (
 
-        <div className="mb-12 animator flex flex-col p-7 items-center justify-center text-white">
-            <div className="w-full md:w-1/2 p-2 text-white">
-                <div className="bg-white/5 p-8 mb-6 rounded-lg text-sm">
-                    <h1 className="raleway-heavy text-center mb-5 text-[2rem]">bio</h1>
-                    <p className="mb-3">
-                        Hello! I'm a dedicated junior software developer with years of professional experience in crafting solutions to solve problems. My background is primarily in the real estate and construction domains, but I am bringing the same discipline and creativity to my emerging software career.
-                    </p>
-                    <p className="mb-3">
-                        On a mission to expand my knowledge and horizons, I completed a part-time engineering bootcamp at General Assembly in early 2024 while continuing my current job. During the six-month course, I developed an unexpected affinity for creating databases & APIs and the ever-growing power of data. This ultimately led me to earn a certificate in Data Analytics from Google a few months afterward. Both of these programs, along with additional online courses, have refined my skills and gain a deeper understanding of how data and design control the user experience.
-                    </p>
-                    <p className="mb-3">
-                        I have worked on multiple projects utilizing both backend and frontend technologies by the way of numerous frameworks and languages. My skills are strong in JavaScript, Python, and SQL, and I am honing my abilities in each of them every day. I'm committed to growing as a developer and data enthusiast. My current career goal is to leverage my background in relations and project management to find synergistic roles that still push me outside of my comfort zone.
-                    </p>
-                    <p className="text-center">Based out of Houston, TX</p>
-                    <hr className="mt-3 text-white" />
+        <div className="mb-12 animator flex flex-col p-7 items-center justify-center">
+            <div className="flex flex-wrap">
+                <div className="w-full md:w-1/2 p-2">
+                    <div className="bg-white/5 p-8 mb-6 rounded-lg text-sm text-white flex-grow">
+                        <h1 className="raleway-heavy text-center mb-5 text-[2rem]">bio</h1>
+                        <p className="mb-3">
+                            Hello! I'm a dedicated junior software developer with years of professional experience in crafting solutions to solve problems. My background is primarily in the real estate and construction domains, but I am bringing the same discipline and creativity to my emerging software career.
+                        </p>
+                        <p className="mb-3">
+                            On a mission to expand my knowledge and horizons, I completed a part-time engineering bootcamp at General Assembly in early 2024 while continuing my current job. During the six-month course, I developed an unexpected affinity for creating databases & APIs and the ever-growing power of data. This ultimately led me to earn a certificate in Data Analytics from Google a few months afterward. Both of these programs, along with additional online courses, have refined my skills and gain a deeper understanding of how data and design control the user experience.
+                        </p>
+                        <p className="mb-3">
+                            I have worked on multiple projects utilizing both backend and frontend technologies by the way of numerous frameworks and languages. My skills are strong in JavaScript, Python, and SQL, and I am honing my abilities in each of them every day. I'm committed to growing as a developer and data enthusiast. My current career goal is to leverage my background in relations and project management to find synergistic roles that still push me outside of my comfort zone.
+                        </p>
+                        <p className="text-center">Based out of Houston, TX</p>
+                        <hr className="mt-3 text-white" />
+                    </div>
                 </div>
-
-                
-                <div className="bg-white/5 p-8 mb-6 rounded-lg text-sm">
-                    <h1 className="raleway-heavy text-center mb-5 text-[2rem]">skills</h1>
-
-                    <h1 className="raleway-medium select-none font-[1.5rem] text-center mb-2">python</h1>
-                    <div className="flex flex-row justify-center p-3 rounded">
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-python-plain" title="Python"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-django-plain" title="Django"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-flask-original" title="Flask"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-pandas-plain" title="Pandas"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-streamlit-plain" title="Streamlit"></i>
+                <div className="w-full md:w-1/2 p-2">
+                    <div className="bg-white/5 p-8 mb-6 rounded-lg text-sm text-white flex-grow">
+                        <h1 className="raleway-heavy text-center mb-5 text-[2rem]">skills</h1>
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4 text-center">
+                            <i className="text-[4rem] p-2 devicon-python-plain" title="Python"></i>
+                            <i className="text-[4rem] p-2 devicon-javascript-plain" title="Javascript"></i>
+                            <i className="text-[4rem] p-2 devicon-rust-plain" title="Rust"></i>
+                            <i className="text-[4rem] p-2 devicon-django-plain" title="Django"></i>
+                            <i className="text-[4rem] p-2 devicon-flask-original" title="Flask"></i>
+                            <i className="text-[4rem] p-2 devicon-pandas-plain" title="Pandas"></i>
+                            <i className="text-[4rem] p-2 devicon-streamlit-plain" title="Streamlit"></i>
+                            <i className="text-[4rem] p-2 devicon-react-original" title="React"></i>
+                            <i className="text-[4rem] p-2 devicon-redux-original" title="Redux"></i>
+                            <i className="text-[4rem] p-2 devicon-express-original" title="Express"></i>
+                            <i className="text-[4rem] p-2 devicon-nodejs-plain" title="Node"></i>
+                            <i className="text-[4rem] p-2 devicon-css3-plain" title="CSS3"></i>
+                            <i className="text-[4rem] p-2 devicon-bootstrap-plain" title="Bootstrap"></i>
+                            <i className="text-[4rem] p-2 devicon-handlebars-original" title="Handlebars"></i>
+                            <i className="text-[4rem] p-2 devicon-tailwindcss-original" title="Tailwind"></i>
+                            <i className="text-[4rem] p-2 devicon-mysql-original" title="MySQL"></i>
+                            <i className="text-[4rem] p-2 devicon-postgresql-plain" title="PostgreSQL"></i>
+                            <i className="text-[4rem] p-2 devicon-sqlite-plain" title="SQLite"></i>
+                            <i className="text-[4rem] p-2 devicon-mongodb-plain" title="MongoDB"></i>
+                            <i className="text-[4rem] p-2 devicon-mongoose-original" title="Mongoose"></i>
+                            <i className="text-[4rem] p-2 devicon-rstudio-plain" title="R Studio"></i>
+                            <i className="text-[4rem] p-2 devicon-matlab-plain" title="Matlab"></i>
+                            <i className="text-[4rem] p-2 devicon-jupyter-plain" title="Jupyter"></i>
+                            <i className="text-[4rem] p-2 devicon-plotly-plain" title="Plotly"></i>
+                            <i className="text-[4rem] p-2 devicon-heroku-original" title="Heroku"></i>
+                            <i className="text-[4rem] p-2 devicon-docker-plain" title="Docker"></i>
+                            <i className="text-[4rem] p-2 devicon-postman-plain" title="Postman"></i>
+                            <i className="text-[4rem] p-2 devicon-amazonwebservices-plain-wordmark" title="AWS"></i>
+                        </div>
+  
+                        <hr className="mt-3 text-white" />
                     </div>
-                  
-            
-                    <h1 className="raleway-medium select-none font-[1.5rem] text-center mb-2">javascript</h1>
-                    <div className="flex flex-row justify-center p-3 rounded">
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-javascript-plain" title="Javascript"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-react-original" title="React"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-redux-original" title="Redux"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-express-original" title="Express"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-nodejs-plain" title="Node"></i>
-                    </div>
-            
-
-                    <h1 className="raleway-medium select-none font-[1.5rem] text-center mb-2">styling</h1>
-                    <div className="flex flex-row justify-center p-3 rounded">
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-css3-plain" title="CSS3"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-bootstrap-plain" title="Bootstrap"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-handlebars-original" title="Handlebars"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-tailwindcss-original" title="Tailwind"></i>
-                    </div>
-        
-                    <h1 className="raleway-medium select-none font-[1.5rem] text-center mb-2">databases</h1>
-                    <div className="flex justify-center p-3 rounded">
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-mysql-original" title="MySQL"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-postgresql-plain" title="PostgreSQL"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-sqlite-plain" title="SQLite"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-mongodb-plain" title="MongoDB"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-mongoose-original" title="Mongoose"></i>
-                    </div>
-            
-                    <h1 className="raleway-medium select-none font-[1.5rem] text-center mb-2">analytics</h1>
-                    <div className="flex justify-center p-3 rounded">
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-rstudio-plain" title="R Studio"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-matlab-plain" title="Matlab"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-jupyter-plain" title="Jupyter"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-plotly-plain" title="Plotly"></i>
-                    </div>
-            
-                    <h1 className="raleway-medium select-none font-[1.5rem] text-center mb-2">deployment</h1>
-                    <div className="flex justify-center p-3 rounded">
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-heroku-original" title="Heroku"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-docker-plain" title="Docker"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-postman-plain" title="Postman"></i>
-                        <i className="text-[4rem] p-2 flex-shrink-1 devicon-amazonwebservices-plain-wordmark" title="AWS"></i>
-                    </div>
-
-                    <hr className="mt-3 text-white" />
                 </div>
-            </div>
+            </div>           
         </div>
  
     )
--- src/elements/Tabs.jsx
+++ src/elements/Tabs.jsx
@@ -11,11 +11,9 @@ function Tabs () {
     const basePath = location.pathname.split('/').pop();
 
     return (
-        <div className='flex flex-col items-center justify-center'>
-            <div className='flex flex-col items-center text-white justify-center'>
-                <a href="/"><h1 className='raleway-heavy text-[4rem] select-none hover:text-white/5'>portfolio</h1></a>
-                <p className='jersey-25-charted-regular text-[1.5rem] select-none -mt-2 uppercase'>jaxon southern</p>                      
-            </div>
+        <div className='flex flex-col items-center justify-center text-white'>
+            <a href="/"><h1 className='raleway-heavy text-[4rem] select-none hover:text-white/5'>portfolio</h1></a>
+            <p className='jersey-25-charted-regular text-[1.5rem] select-none -mt-2 uppercase'>jaxon southern</p>                      
             <div className='flex flex-row items-center justify-center bg-white text-[#e7a55e] rounded-lg p-2 mt-3'>
                 <Link className='pl-2 pr-2 raleway-medium hover:text-white' to="/about">about</Link>
                 <Link className='pl-2 pr-2 raleway-medium hover:text-white' to="/">projects</Link>
