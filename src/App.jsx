d1a34c474262243b49a39d30d88339a38071e267
jaxdsout <jaxon.southern@mac.com>
2025-02-10 17:13:16 -0600

    flex grow bug

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,16 @@ function App () {
 
     return (
         <div className='flex flex-col items-center justify-center w-screen'>
-            <Routes>
-                <Route path="/" element={<Tabs />}>
-                    <Route path="" element={<Projects />} />
-                    <Route path="about" element={<About />} />
-                    <Route path="contact" element={<Contact />} />
-                </Route>
-            </Routes>
+            <div className='flex flex-col items-center justify-center max-w-[1400px]'>
+                <Routes>
+                    <Route path="/" element={<Tabs />}>
+                        <Route path="" element={<Projects />} />
+                        <Route path="about" element={<About />} />
+                        <Route path="contact" element={<Contact />} />
+                    </Route>
+                </Routes>
+            </div>
+            
             <Motion />
         </div>
     )
