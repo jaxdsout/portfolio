b0692bb490c82df6b1a61e4ff5526ab05d33a9bb
jaxdsout <jaxon.southern@mac.com>
2025-04-03 16:02:43 -0500

    resized page; transitions for colors

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -34,28 +34,25 @@ function Content () {
             thumbnail = AtlasThumb;
             accent = '#5F85DB';
             content = (
-                <>
-                    <p className="pb-4">
-                        Apartment locators are real estate agents who specialize in leasing within corporate apartment communities and buildings. While the listings they recommend are publicly available online, there’s no streamlined way to share them with clients. Some locators rely on outdated systems, while others simply compile links into an email. This challenge led to the creation of <b>Atlas.</b>
-                    </p>
-                    <p className="pb-4">
-                        At its core, Atlas is a powerful list-making tool. However, it was built with scalability in mind, and new features have been continuously integrated since its initial development. The platform now includes deal (invoice) tracking, monthly earnings tables, a task manager, and a net-effective rent calculator. The latest addition is a commissions database, which helps users track current payout rates for apartment communities in their area.
-                    </p>
-                    <p className="pb-4">
-                        The web app is built with React and Redux, ensuring a dynamic and efficient user experience with smooth state management. It leverages Django for a custom backend API and uses Tailwind CSS to create a visually distinct and modern interface.
-                    </p>
-                    <p className="pb-4">
-                        To login and access sandbox account, use the following credentials (click & they will be copied to your clipboard):
-                    </p>
-                    <div className="flex flex-col items-center justify-center relative mt-2">
-                        {copied ? <i className="text-xs absolute -top-5 text-red-300 text-nowrap transition-opacity duration-300">COPIED TO CLIPBOARD</i> : " "}
-                        <b onClick={() => copyClipboard('USERNAME')} className="mb-2">USERNAME</b>
-                        <b onClick={() => copyClipboard('PASSWORD')}>PASSWORD</b>
-                    </div>
-                  
-                    <hr className="mt-3 text-white" />
-
-                </>            
+                <p className="pb-4">
+                    Apartment locators are real estate agents who specialize in leasing within corporate apartment communities and buildings. While the listings they recommend are publicly available online, there’s no streamlined way to share them with clients. Some locators rely on outdated systems, while others simply compile links into an email. This challenge led to the creation of <b>Atlas.</b>
+                    <br></br>
+                    <br></br>
+                    At its core, Atlas is a powerful list-making tool. However, it was built with scalability in mind, and new features have been continuously integrated since its initial development. The platform now includes deal (invoice) tracking, monthly earnings tables, a task manager, and a net-effective rent calculator. The latest addition is a commissions database, which helps users track current payout rates for apartment communities in their area.
+                    <br></br>
+                    <br></br>
+                    The web app is built with React and Redux, ensuring a dynamic and efficient user experience with smooth state management. It leverages Django for a custom backend API and uses Tailwind CSS to create a visually distinct and modern interface.
+                    <br></br>
+                    <br></br>
+                    To login and access sandbox account, use the following credentials (click & they will be copied to your clipboard):
+                    <br></br>
+                    <br></br>
+                    {copied ? <i className="text-xs absolute -top-5 text-red-300 text-nowrap transition-opacity duration-300">COPIED TO CLIPBOARD</i> : " "}
+                    <b onClick={() => copyClipboard('USERNAME')} className="mb-2">USERNAME</b>
+                    <br></br>
+                    <br></br>
+                    <b onClick={() => copyClipboard('PASSWORD')}>PASSWORD</b>
+                </p>
             );
             break;
         case '2':
@@ -64,17 +61,15 @@ function Content () {
             thumbnail = SubstreamThumb;
             accent = '#a5d294';
             content = (
-                <>
-                    <p className="pb-4">
-                        Trying to find where a specific TV show or movie is streaming nowadays is a headache. Most people pay for multiple apps or services on top of free, ad-supported platforms. So searching through each one for <i>Teenage Mutant Ninja Turtles II: The Secret of the Ooze</i> or any content can be a lengthy—and sometimes fruitless—process. The solution? <b>Substream.</b>
-                    </p>
-                    <p className="pb-4">
-                        This project is a streamlined query tool that allows users to search for a title and instantly see where it’s currently available. It provides direct links to the content on each respective streaming service, as well as links to its IMDb and Letterboxd pages.
-                    </p>
-                    <p>
-                        The web app leverages React and Redux to create a seamless user experience, enabling asynchronous data retrieval and smooth state management. It utilizes the Watchmode API for all media content and descriptions, combining Semantic UI and Tailwind CSS to deliver a distinct and polished interface.
-                    </p>
-                </>
+                <p className="pb-4">
+                    Trying to find where a specific TV show or movie is streaming nowadays is a headache. Most people pay for multiple apps or services on top of free, ad-supported platforms. So searching through each one for <i>Teenage Mutant Ninja Turtles II: The Secret of the Ooze</i> or any content can be a lengthy—and sometimes fruitless—process. The solution? <b>Substream.</b>
+                    <br></br>
+                    <br></br>
+                    This project is a streamlined query tool that allows users to search for a title and instantly see where it’s currently available. It provides direct links to the content on each respective streaming service, as well as links to its IMDb and Letterboxd pages.
+                    <br></br>
+                    <br></br>
+                    The web app leverages React and Redux to create a seamless user experience, enabling asynchronous data retrieval and smooth state management. It utilizes the Watchmode API for all media content and descriptions, combining Semantic UI and Tailwind CSS to deliver a distinct and polished interface.
+                </p>
             );
             break;
         case '3':
@@ -83,14 +78,12 @@ function Content () {
             thumbnail = GalleryThumb;
             accent = '#464646';
             content = (
-              <>
-                    <p className="pb-4">
-                        This project serves as both the customer-facing website and inventory system for a fictional art gallery. The backend is a custom API that manages inventory, artist information, event calendars, and bid logs. The gallery operates entirely on a bidding system, similar to eBay, but without a “Buy Now” option. As a pro-artist establishment, they aim to ensure that each piece sells for its true value. If an artwork doesn’t sell, the gallery owner or artist can decide whether to renew the listing, utilizing the optional "auto-renew" feature.
-                    </p>
-                    <p>
-                        The web app is built with React on the frontend and Django on the backend. All media was sourced from my Midjourney account, while the interface combines Tailwind CSS with select elements from Semantic UI and is serviced by an S3 bucket on AWS. The interface is a result of Tailwind and some elements of Semantic UI.
-                    </p>
-                </>
+                <p className="pb-4">
+                    This project serves as both the customer-facing website and inventory system for a fictional art gallery. The backend is a custom API that manages inventory, artist information, event calendars, and bid logs. The gallery operates entirely on a bidding system, similar to eBay, but without a “Buy Now” option. As a pro-artist establishment, they aim to ensure that each piece sells for its true value. If an artwork doesn’t sell, the gallery owner or artist can decide whether to renew the listing, utilizing the optional "auto-renew" feature.
+                    <br></br>
+                    <br></br>
+                    The web app is built with React on the frontend and Django on the backend. All media was sourced from my Midjourney account, while the interface combines Tailwind CSS with select elements from Semantic UI and is serviced by an S3 bucket on AWS. The interface is a result of Tailwind and some elements of Semantic UI.
+                </p>
             );
             break;
         default:
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,8 +14,8 @@ function Project ({ title, children, links, thumbnail, accent }) {
     }
 
     return (
-        <div className={`mb-12 flex flex-col p-7 items-center justify-center transition-opacity duration-300 ${clicked ? "opacity-0" : "opacity-100"}`}>
-            <div className="mb-4">
+        <div className={`flex flex-col p-7 items-center justify-center transition-opacity duration-300 ${clicked ? "opacity-0" : "opacity-100"}`}>
+            <div>
                 <button 
                     className='ml-4 mr-4 transition ease-out' 
                     style={{ color: hovered === "back" ? accent : "white" }} 
@@ -31,44 +31,54 @@ function Project ({ title, children, links, thumbnail, accent }) {
                     </svg>
                 </button>
             </div>
-            <div className="w-11/12 md:w-1/2 p-4 min-w-[350px] md:min-w-[500px]">
+            <div className="w-11/12 md:w-1/2 p-1 min-w-[350px] md:min-w-[500px]">
                 <div className="bg-white/5 p-8 mb-6 rounded-lg text-sm text-white border-[0.05rem] border-white/5">
-                    <div className="flex flex-col mt-3 items-start p-3 rounded-lg bg-white/5 text-white border-[0.05rem] border-white/5">
-                        <div className="flex flex-row items-start justify-between w-full">            
-                            <h3 className="font-ralewayBlack font-black tracking-wider text-[1.2rem] ml-1"> {title} </h3>
-                            <div className="flex flex-row items-start">
-                                <a href={links[1]} target="_blank" rel="noopener noreferrer" style={{ color: hovered === "repo" ? accent : "white" }} onMouseEnter={() => setHovered('repo')} onMouseLeave={() => setHovered(null)} >
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 -mt-1 mr-1">
-                                        <path 
-                                            fillRule="evenodd" clipRule="evenodd" 
-                                            d="M3 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm14.25 6a.75.75 0 0 1-.22.53l-2.25 2.25a.75.75 0 1 1-1.06-1.06L15.44 12l-1.72-1.72a.75.75 0 1 1 1.06-1.06l2.25 2.25c.141.14.22.331.22.53Zm-10.28-.53a.75.75 0 0 0 0 1.06l2.25 2.25a.75.75 0 1 0 1.06-1.06L8.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-2.25 2.25Z" 
-                                            
-                                        />
-                                    </svg>
-                                </a>
-                                <a href={links[0]} target="_blank" rel="noopener noreferrer" style={{ color: hovered === "link" ? accent : "white" }} onMouseEnter={() => setHovered('link')} onMouseLeave={() => setHovered(null)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-                                        <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707"/>
-                                    </svg>
-                                </a>
-                            </div>
+                    <div className="flex flex-row items-center justify-between w-full mt-3 items-start p-3 rounded-lg bg-white/5 text-white border-[0.05rem] border-white/5">
+                        <h3 className="font-ralewayBlack font-black tracking-wider text-[1.2rem] ml-1"> {title} </h3>
+                        <div className="flex flex-row items-start">
+                            <a 
+                                href={links[1]} 
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                style={{ color: hovered === "repo" ? accent : "white" }} 
+                                onMouseEnter={() => setHovered('repo')} 
+                                onMouseLeave={() => setHovered(null)} 
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 -mt-1 mr-1">
+                                    <path 
+                                        fillRule="evenodd" clipRule="evenodd" 
+                                        d="M3 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm14.25 6a.75.75 0 0 1-.22.53l-2.25 2.25a.75.75 0 1 1-1.06-1.06L15.44 12l-1.72-1.72a.75.75 0 1 1 1.06-1.06l2.25 2.25c.141.14.22.331.22.53Zm-10.28-.53a.75.75 0 0 0 0 1.06l2.25 2.25a.75.75 0 1 0 1.06-1.06L8.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-2.25 2.25Z" 
+                                        
+                                    />
+                                </svg>
+                            </a>
+                            <a 
+                                href={links[0]} 
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                style={{ color: hovered === "link" ? accent : "white" }} 
+                                onMouseEnter={() => setHovered('link')} 
+                                onMouseLeave={() => setHovered(null)}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
+                                    <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707"/>
+                                </svg>
+                            </a>
                         </div>
-                        <hr className="mt-2 text-white w-full" />
                     </div>
                     <div className="w-full flex flex-col items-center">
                         <img
-                        src={thumbnail}
-                        className=" block w-full rounded-2xl drop-shadow-sm transition-opacity duration-300 cursor-pointer mt-2 active:translate-y-1"
-                        alt="thumbnail"
-                        onDoubleClick={() => window.open(links[0])}
-                        onClick={() => window.open(links[0])}
+                            src={thumbnail}
+                            className=" block w-full rounded-2xl drop-shadow-sm transition-opacity duration-300 cursor-pointer mt-2 active:translate-y-1"
+                            alt="thumbnail"
+                            onDoubleClick={() => window.open(links[0])}
+                            onClick={() => window.open(links[0])}
                         />
                     </div>
-                    <div className="flex flex-col items-center">
-                        <div className="flex flex-col items-center text-center py-3 px-6 my-3 bg-black/40 rounded-xl text-sm">
+                    <div className="flex flex-col items-center text-center py-3 px-6 mt-3 bg-black/40 rounded-xl text-sm h-[350px] auto-scrollbar">
                             {children}
                             <hr className="mt-3 mb-3 text-white w-full" />
-                        </div>
+                    
                     </div>
                 </div>
             </div>
