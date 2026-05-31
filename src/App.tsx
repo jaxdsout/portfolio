9c864baa6013e6b0254b0de2e07ef45724077b44
jaxdsout <jaxon.southern@mac.com>
2026-05-31 18:18:54 -0500

    bg ruminate

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ function App(): JSX.Element {
         </AnimatePresence>
       </div>
 
-      <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100dvh', zIndex: 1 }}>
+      <div style={{ position: 'fixed', inset: 0, zIndex: 1 }}>
         <Background />
       </div>
     </>
